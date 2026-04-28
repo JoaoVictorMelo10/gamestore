371481df11ecc5a27d797ee86f39a9935c030451
João Victor <melo.joaovitor10@gmail.com>
2026-04-27 22:42:49 -0300

    feat: configura reat-router-dom e estrutura de rotas no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Error from './pages/Error'
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Header />
+        <main style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
